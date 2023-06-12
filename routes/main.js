71090f28d45af139608205fbffc124f6f25cec99
AntonKolomiiets <anton.kolomiiets@logz.io>
2023-06-12 12:05:29 +0300

    remove old files

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const mainController = require('../controllers/main');
-
-const router = express.Router();
-
-router.get('/', mainController.homepage);
-
-module.exports = router;
